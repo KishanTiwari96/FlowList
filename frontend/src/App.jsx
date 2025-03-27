a7d58e9c006b04e6ed86d56a94c626f25f8e5bef
KishanTiwari96 <kishantiwari698591@gmail.com>
2025-03-27 21:45:19 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -23,7 +23,7 @@ function AppBar(){
   const navigate = useNavigate();
   return (
     <div className="font-semibold text-center bg-black h-screen w-screen">
-      <h1 className="mt-6 text-8xl pt-4 bg-gradient-to-r from-rose-400 via-fuchsia-500 to-indigo-500 bg-clip-text text-transparent">To-Do App</h1>      
+      <h1 className="mt-6 text-8xl pt-4 bg-gradient-to-r from-rose-400 via-fuchsia-500 to-indigo-500 bg-clip-text text-transparent">Flow List</h1>      
       <button className="border border-black-500 mt-20 text-2xl p-2  bg-gray-500 hover:bg-blue-500 rounded-md" onClick={()=>{
         navigate("/signup")
       }}>Sign Up</button>
